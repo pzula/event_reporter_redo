8495901451ce8349662a6fb85b83c48f8fbf5100
Persa Zula <persa@persazula.com>
2013-10-07 11:39:38 -0600

    First tests passing... slowly subbing in real code next

---
+++ lib/attendee_parser.rb
@@ -0,0 +1,40 @@
+class AttendeeParser
+
+  attr_reader :attendees
+  
+  def parse_file(filename)
+
+    a = Attendee.new(:first_name => "Sam",
+                    :last_name => "Johnson",
+                    :email => "sam@gmail.com",
+                    :home_phone => "720-888-888",
+                    :street => "123 First Street",
+                    :city => "Denver",
+                    :state => "CO",
+                    :zipcode => "80204")
+
+    @attendees = [a]
+    10
+  end
+
+end
+
+
+class Attendee
+
+  attr_accessor :first_name, :last_name,
+                :email, :home_phone, :street,
+                :city, :state, :zipcode
+
+
+  def initialize(data)
+    @first_name     = data[:first_name]
+    @last_name      = data[:last_name]
+    @email          = data[:email]
+    @home_phone     = data[:home_phone]
+    @street         = data[:street]
+    @city           = data[:city]
+    @state          = data[:state]
+    @zipcode        = data[:zipcode]
+  end
+end
---
+++ test/attendee_parser_test.rb
@@ -0,0 +1,32 @@
+gem 'minitest'
+require 'minitest/autorun'
+require 'minitest/pride'
+require './lib/attendee_parser'
+
+class AttendeeParserTest < Minitest::Test 
+
+  def test_it_exists
+    assert AttendeeParser
+  end
+
+  def test_it_parses_a_data_file
+    ap = AttendeeParser.new
+    attendees_parsed = ap.parse_file('/test/fixtures/partial_attendees.csv')
+    assert_equal 10, attendees_parsed
+  end
+
+  def test_the_attendee_data_is_accessible
+    ap = AttendeeParser.new
+    attendees_parsed = ap.parse_file('/test/fixtures/partial_attendees.csv')
+    attendee = ap.attendees.first
+    assert_equal "Sam", attendee.first_name
+    assert_equal "Johnson", attendee.last_name
+    assert_equal "sam@gmail.com", attendee.email
+    assert_equal "720-888-888", attendee.home_phone
+    assert_equal "123 First Street", attendee.street
+    assert_equal "Denver", attendee.city
+    assert_equal "CO", attendee.state
+    assert_equal "80204", attendee.zipcode 
+  end
+
+end
