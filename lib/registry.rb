536aab5074ffb8109222b380901033e45403a78b
Persa Zula <persa@persazula.com>
2013-10-07 13:25:09 -0600

    Get the test to work...

--- lib/registry.rb
+++ lib/registry.rb
@@ -1,6 +1,11 @@
-class Registry
-  def find_all_by_first_name(name)
+require './lib/attendee'
 
+class Registry
+  
+  attr_accessor :attendees
+
+  def find_all_by_first_name(name)
+    attendees
   end
 
 
--- test/registry_test.rb
+++ test/registry_test.rb
@@ -2,15 +2,21 @@ gem 'minitest'
 require 'minitest/autorun'
 require 'minitest/pride'
 require './lib/registry'
+require './lib/attendee'
 
 class RegistryTest < Minitest::Test 
-  def test_it_exits
+  def test_it_exists
     assert Registry
   end
 
   def test_it_finds_attendees_by_first_name
     #setup object and data
     reg = Registry.new
+    reg.attendees = [
+      Attendee.new(:first_name => "Sarah"),
+      Attendee.new(:first_name => "Sarah"),
+      Attendee.new(:first_name => "Billy")
+    ]
 
     # do the thing
     attendees = reg.find_all_by_first_name("Sarah")
