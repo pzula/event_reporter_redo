d022c6f80de9878463fb23a03df943902c0c8c7b
Persa Zula <persa@persazula.com>
2013-10-07 13:17:17 -0600

    Sets up Registry class and test

--- test/registry_test.rb
+++ test/registry_test.rb
@@ -10,10 +10,16 @@ class RegistryTest < Minitest::Test
 
   def test_it_finds_attendees_by_first_name
     #setup object and data
+    reg = Registry.new
 
     # do the thing
+    attendees = reg.find_all_by_first_name("Sarah")
 
     # assert that you got the thing
+    assert_equal 2, attendees.count
+    attendees.each do |attendee|
+      assert_equal "Sarah", attendee.first_name 
+    end
 
   end 
 
