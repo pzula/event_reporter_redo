558cb8c6bd8d042d76d9f447b9a1045af64a5e7b
Persa Zula <persa@persazula.com>
2013-10-08 11:24:25 -0600

    Another way to do the same thing with the fields

--- lib/printer.rb
+++ lib/printer.rb
@@ -5,18 +5,21 @@ class Printer
   end
 
   def print_attendee(attendee)
-    #pull the attributes
-    # make sure they're in the right order
-    # put it in a string with "  " in between
-    [attendee.last_name,
-      attendee.first_name,
-      attendee.email,
-      attendee.zipcode,
-      attendee.city,
-      attendee.state,
-      attendee.street,
-      attendee.home_phone
-    ].join("  ")
+    fields = [:last_name, :first_name, :email, :zipcode, :city, :state, :home_phone]
+    fields.collect do |attribue|
+      attendee.send(attribue)
+    end.join("  ")
+
+
+    # [attendee.last_name,
+    #   attendee.first_name,
+    #   attendee.email,
+    #   attendee.zipcode,
+    #   attendee.city,
+    #   attendee.state,
+    #   attendee.street,
+    #   attendee.home_phone
+    # ].join("  ")
 
     "Schmoe Joe  joe@example.com  80209 Denver  CO  123 First Street  202-555-5555"
   end
