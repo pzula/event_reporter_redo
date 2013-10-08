ff3d9529072cd33bd032e3ef295759792de6dbcb
Persa Zula <persa@persazula.com>
2013-10-08 14:18:19 -0600

    Runs queue with more than one command

--- lib/command_interpreter.rb
+++ lib/command_interpreter.rb
@@ -25,9 +25,12 @@ class CommandInterpreter
       filename = parts[1] || default_filename
       runner.load(filename)
     elsif instruction == "queue"
-      #Queue print
-      order = parts.last
-      runner.queue_print(order)
+      if parts[1] == "print"
+        order = parts.last
+        runner.queue_print(order)
+      elsif parts[1] == "count"
+        runner.queue_count
+      end
     end
   end
 
--- test/command_interpreter_test.rb
+++ test/command_interpreter_test.rb
@@ -18,6 +18,10 @@ class CommandInterpreterTest < Minitest::Test
     def queue_print(order)
       "running queue print ordered by #{order}"
     end
+
+    def queue_count
+      "running queue count"
+    end
   end
 
   def test_it_accepts_a_load_instruction
@@ -35,6 +39,11 @@ class CommandInterpreterTest < Minitest::Test
   def test_it_prints_ordered_by_the_last_name
     result = ci.run("queue print by last_name")
     assert_equal "running queue print ordered by last_name", result
+  end
+
+  def test_it_counts_the_queue
+    result = ci.run("queue count")
+    assert_equal "running queue count", result
   end
 
 end
