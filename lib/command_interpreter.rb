b0e150b9a0889717a74091fa0210db074061a943
Persa Zula <persa@persazula.com>
2013-10-08 13:24:03 -0600

    Makes the test pass, but doesn't achieve the goal

--- lib/command_interpreter.rb
+++ lib/command_interpreter.rb
@@ -1,4 +1,13 @@
 class CommandInterpreter
+  
+  def run(command)
+    # load file.csv
+    parts = command.split
+    instruction = parts[0]
+    filename = parts[1]
 
+    [instruction.to_sym, filename]
+
+  end
 
 end
