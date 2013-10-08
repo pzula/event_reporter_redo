0de91eca44915af1a9fa23ebaf51d5b4f8776548
Persa Zula <persa@persazula.com>
2013-10-08 11:51:03 -0600

    Refactors test for readability

--- test/printer_test.rb
+++ test/printer_test.rb
@@ -16,38 +16,38 @@ class PrinterTest < Minitest::Test
     assert_equal expected_header, printer.header
   end 
 
+  def attendee_joe
+    Attendee.new( :first_name => "Joe",
+                  :last_name => "Schmoe",
+                  :email => "joe@example.com",
+                  :zipcode => "80209",
+                  :city => "Denver",
+                  :state => "CO",
+                  :street => "123 First Street",
+                  :home_phone => "202-555-5555" )
+  end
+
+  def attendee_jane
+    Attendee.new( :first_name => "Jane",
+                  :last_name => "Schmoe",
+                  :email => "jane@example.com",
+                  :zipcode => "80209",
+                  :city => "Denver",
+                  :state => "CO",
+                  :street => "123 First Street",
+                  :home_phone => "202-777-7777" )
+  end
+
   def test_it_prints_an_attendee
-    attendee = Attendee.new( :first_name => "Joe",
-                            :last_name => "Schmoe",
-                            :email => "joe@example.com",
-                            :zipcode => "80209",
-                            :city => "Denver",
-                            :state => "CO",
-                            :street => "123 First Street",
-                            :home_phone => "202-555-5555" )
     printer = Printer.new
     expected = "Schmoe  Joe  joe@example.com  80209  Denver  CO  123 First Street  202-555-5555"
-    assert_equal expected, printer.print_attendee(attendee)
+    assert_equal expected, printer.print_attendee(attendee_joe)
   end
 
   def test_it_can_print_multiple_attendees
     attendees = [
-              Attendee.new( :first_name => "Joe",
-                            :last_name => "Schmoe",
-                            :email => "joe@example.com",
-                            :zipcode => "80209",
-                            :city => "Denver",
-                            :state => "CO",
-                            :street => "123 First Street",
-                            :home_phone => "202-555-5555" ),
-              Attendee.new( :first_name => "Jane",
-                            :last_name => "Schmoe",
-                            :email => "jane@example.com",
-                            :zipcode => "80209",
-                            :city => "Denver",
-                            :state => "CO",
-                            :street => "123 First Street",
-                            :home_phone => "202-777-7777" )]
+              attendee_joe,
+              attendee_jane]
     printer = Printer.new
     expected1 = "Schmoe  Joe  joe@example.com  80209  Denver  CO  123 First Street  202-555-5555"
     expected2 = "Schmoe  Jane  jane@example.com  80209  Denver  CO  123 First Street  202-777-7777"
