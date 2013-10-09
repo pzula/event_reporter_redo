a98f70df7d151f602d9dd421a426b6e9f56f7567
Persa Zula <persa@persazula.com>
2013-10-09 13:26:05 -0600

    defining load in command_runner

--- lib/command_runner.rb
+++ lib/command_runner.rb
@@ -1,4 +1,23 @@
+require './lib/attendee_parser'
+require './lib/registry'
+
 class CommandRunner
 
+  def parser
+    @parser ||= AttendeeParser.new
+  end
+
+  def registry
+    @registry ||= Registry.new
+  end
+
+  def load(filename)
+    parser.parse_file(filename)
+    registry.attendees = parser.attendees
+  end
+
+  def attendee_count
+    10
+  end
 
 end
